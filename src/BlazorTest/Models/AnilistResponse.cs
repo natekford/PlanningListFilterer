87b0d9379bcfe4fed3687beb90de0472bf723b08
Advorange <advorange@live.com>
2022-11-04 22:42:45 -0700

    Moved a lot of the anilist graphql query logic to the model as a static method.
    Null values are now always shown at the end when sorted.

--- src/BlazorTest/Models/AnilistResponse.cs
+++ src/BlazorTest/Models/AnilistResponse.cs
@@ -1,11 +1,96 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace BlazorTest.Models;
 
 public sealed record AnilistResponse(
 	[property: JsonPropertyName("data")]
 	Data Data
-);
+)
+{
+	internal static JsonSerializerOptions JsonOptions { get; } = CreateJsonOptions();
+
+	public const string GRAPHQL_URL = "https://graphql.anilist.co";
+	public const string GRAPHQL_QUERY = @"
+	query ($username: String) {
+		MediaListCollection(userName: $username, type: ANIME, status: PLANNING) {
+			lists {
+				name
+				isCustomList
+				isCompletedList: isSplitCompletedList
+				entries {
+					...mediaListEntry
+				}
+			}
+		}
+	}
+
+	fragment mediaListEntry on MediaList {
+		media {
+			id
+			title {
+				userPreferred
+			}
+			format
+			episodes
+			nextAiringEpisode {
+				episode
+			}
+			duration
+			averageScore
+			popularity
+			startDate {
+				year
+			}
+		}
+	}
+	";
+
+	public static async Task<AnilistResponse> GetAnilistResponseAsync(
+		HttpClient client,
+		string username)
+	{
+		var body = JsonSerializer.Serialize(new
+		{
+			query = GRAPHQL_QUERY,
+			variables = new
+			{
+				username = username,
+			}
+		});
+		var content = new StringContent(
+			content: body,
+			encoding: Encoding.UTF8,
+			mediaType: MediaTypeNames.Application.Json
+		);
+
+		var request = new HttpRequestMessage(
+			method: HttpMethod.Post,
+			requestUri: GRAPHQL_URL
+		)
+		{
+			Content = content
+		};
+		request.Headers.Add("Accept", MediaTypeNames.Application.Json);
+
+		using var response = await client.SendAsync(request).ConfigureAwait(false);
+		using var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+		return (await JsonSerializer.DeserializeAsync<AnilistResponse>(
+			utf8Json: stream,
+			options: JsonOptions
+		).ConfigureAwait(false))!;
+	}
+
+	private static JsonSerializerOptions CreateJsonOptions()
+	{
+		var options = new JsonSerializerOptions();
+		options.Converters.Add(new JsonStringEnumConverter());
+		return options;
+	}
+};
 
 public sealed record Data(
 	[property: JsonPropertyName("MediaListCollection")]
