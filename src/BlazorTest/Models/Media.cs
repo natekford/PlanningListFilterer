36993664b80764e7f0f510679026487c4df0053b
Advorange <advorange@live.com>
2022-11-05 18:03:03 -0700

    Switched some properties on Media to extension methods.
    Moved some usings to _Imports.

--- src/BlazorTest/Models/Media.cs
+++ src/BlazorTest/Models/Media.cs
@@ -21,9 +21,40 @@ public sealed record Media(
 	int Popularity,
 	[property: JsonPropertyName("startDate")]
 	StartDate? StartDate
-)
+);
+
+public static class MediaUtils
 {
-	public string Url => $"https://anilist.co/anime/{Id}/";
-	public int? HighestAvailableEpisode => Episodes ?? NextAiringEpisode?.Episode;
-	public int? TotalDuration => HighestAvailableEpisode * Duration;
+	public static string DisplayDuration(this Media media)
+	{
+		var duration = media.GetTotalDuration() ?? 0;
+		return $"{duration} minute{(duration == 1 ? "" : "s")}";
+	}
+
+	public static string DisplayEpisodeCount(this Media media)
+	{
+		var count = media.GetHighestEpisode() ?? 0;
+		return $"{count} episode{(count == 1 ? "" : "s")}";
+	}
+
+	public static string DisplayFormat(this Media media)
+		=> media.Format?.ToString() ?? "Unknown";
+
+	public static string DisplayScore(this Media media)
+	{
+		var score = media.AverageScore ?? 0;
+		return $"{score}%";
+	}
+
+	public static string DisplayYear(this Media media)
+		=> (media.StartDate?.Year ?? 0).ToString();
+
+	public static int? GetHighestEpisode(this Media media)
+		=> media.Episodes ?? media.NextAiringEpisode?.Episode;
+
+	public static int? GetTotalDuration(this Media media)
+		=> media.GetHighestEpisode() * media.Duration;
+
+	public static string GetUrl(this Media media)
+		=> $"https://anilist.co/anime/{media.Id}/";
 }
