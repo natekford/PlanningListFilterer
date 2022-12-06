25eb88d4353b95474a24fd2d14e25db5cc139cce
Advorange <advorange@live.com>
2022-12-06 03:57:36 -0800

    JsonEditor now simplifies Json further by removing all empty properties.

--- src/BlazorTest/Pages/JsonEditor.razor.cs
+++ src/BlazorTest/Pages/JsonEditor.razor.cs
@@ -3,6 +3,9 @@ using Json.Schema;
 
 using Microsoft.JSInterop;
 
+using MudBlazor.Extensions;
+
+using System.Collections;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Nodes;
@@ -27,7 +30,7 @@ public partial class JsonEditor
 
 	public static bool RemoveDefaults(JsonNode? @default, JsonNode? node)
 	{
-		// node == defaults even if both ar enull
+		// node == defaults even if both are null
 		if (@default is null)
 		{
 			return node is null;
@@ -44,53 +47,81 @@ public partial class JsonEditor
 				$"Defaults and current JSON node are not the same type at '{node.GetPath()}'.");
 		}
 
-		var isDefaultValue = true;
 		// loop through all array values
 		if (@default is JsonArray jDefaultArray && node is JsonArray jArray)
 		{
-			isDefaultValue = jArray.Count == jDefaultArray.Count;
 			for (var i = jDefaultArray.Count - 1; i >= 0; --i)
 			{
 				if (RemoveDefaults(jDefaultArray[i], jArray[i]))
 				{
 					jArray.RemoveAt(i);
 				}
-				else
-				{
-					isDefaultValue = false;
-				}
 			}
+			return RemoveEmpty(jArray);
 		}
 		// loop through all properties
 		else if (@default is JsonObject jDefaultObj && node is JsonObject jObj)
 		{
-			isDefaultValue = jObj.All(x => jDefaultObj.ContainsKey(x.Key));
 			foreach (var (propertyName, jDefaultProperty) in jDefaultObj)
 			{
 				if (RemoveDefaults(jDefaultProperty, jObj[propertyName]))
 				{
 					jObj.Remove(propertyName);
 				}
-				else
-				{
-					isDefaultValue = false;
-				}
 			}
+			return RemoveEmpty(jObj);
 		}
 		// check if json strings are the same
 		else if (@default is JsonValue jDefaultValue && node is JsonValue jValue)
 		{
 			var jDefault = jDefaultValue.AsJsonString();
 			var jString = jValue.AsJsonString();
-			Console.WriteLine($"{jDefault}/{jString}");
-			isDefaultValue = jDefault == jString;
+			return jDefault == jString || RemoveEmpty(jValue);
 		}
 		else
 		{
 			throw new ArgumentException("Not a valid JSON type.", nameof(node));
 		}
+	}
 
-		return isDefaultValue;
+	public static bool RemoveEmpty(JsonNode? node)
+	{
+		if (node is JsonArray jArray)
+		{
+			for (var i = jArray.Count - 1; i >= 0; --i)
+			{
+				if (RemoveEmpty(jArray[i]))
+				{
+					jArray.RemoveAt(i);
+				}
+			}
+			return jArray.Count == 0;
+		}
+		else if (node is JsonObject jObj)
+		{
+			foreach (var (propertyName, jProperty) in jObj.ToList())
+			{
+				if (RemoveEmpty(jProperty))
+				{
+					jObj.Remove(propertyName);
+				}
+			}
+			return jObj.Count == 0;
+		}
+		else if (node is JsonValue jValue)
+		{
+			var jStr = jValue.ToJsonString();
+			// empty strings still get quotation marks around them
+			if (jStr is string s && s.StartsWith('"') && s.EndsWith('"'))
+			{
+				jStr = s[1..^1];
+			}
+			return string.IsNullOrWhiteSpace(jStr);
+		}
+		else
+		{
+			throw new ArgumentException("Not a valid JSON type.", nameof(node));
+		}
 	}
 
 	[JSInvokable]
